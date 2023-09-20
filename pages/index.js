8ef7e0af56a4396130846a667181809d9257f15f
Meenakshi Dhanani <meenakshi.dhanani@postman.com>
2023-09-19 18:04:16 -0700

    update Github repository url for collecting feedback

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,17 @@ export default function Home() {
       <main>
         <Header title="GraphQL Concepts" />
         <p className="description">
-          Collect 5 unique concept cards at the GraphQL Conf to claim some cool Postman swag! 
+        These concepts are the fundamental building blocks of modern GraphQL APIs. Many of them come from the GraphQL Specification, tagged spec, and the others don’t, tagged non-spec. Breaking down complex technology structures into singular concepts is an important technique for simplifying technology, making it more accessible to a wider audience, and improving communication and collaboration across the technology community.
+        </p>
+        <p className='cta'>          
+        Collect 3 unique concept cards at the GraphQL Conf to claim some cool Postman swag!
         </p>
         <iframe className="airtable-embed" 
         src="https://airtable.com/embed/appHEZBQajKXcPlrg/shr18BuXdjn4AdZIn?backgroundColor=red&viewControls=on"
         style={{'background': 'transparent', 'border': '1px solid #ccc'}}>
         </iframe>
         <p className="description">
-          Share your feedback with us on <a href=''>#graphql-concepts</a> Discord channel 
+          Share your feedback with us by creating an issue on <a href='https://github.com/postman-open-technologies/graphql-concepts'>https://github.com/postman-open-technologies/graphql-concepts</a>
         </p>
       </main>
     </div>
